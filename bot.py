cf67902b6980fb0911721871aa68e65a178a610e
cpakhil36-pixel <cpakhil36@gmail.com>
2026-09-07 21:39:38 +0530

    Add quiz data structure for capital question

--- bot.py
+++ bot.py
@@ -1,1 +1,12 @@
-
+data = {
+    "chat_id": CHAT_ID,
+    "question": "Capital of India is?",
+    "options": [
+        "Mumbai",
+        "New Delhi",
+        "Chennai",
+        "Kolkata"
+    ],
+    "type": "quiz",
+    "correct_option_id": 1
+}
